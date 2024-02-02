0d0df9d2d3ede0e2a24103125a5bbab542fb5b77
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-02-02 21:50:46 +0500

    wmvwv

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ if ((year % 4 === 0 && year % 100 != 0) || year % 400 === 0) {
   console.log("Kabisa yil emas");
 }
 console.log(Math.sqrt(81));
-console.log(Math.sqrt(120));
+console.log(Math.sqrt(81));
+console.log(Math.sqrt(1024));
